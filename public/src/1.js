1cd92e360a5e1b1c87992fe2bace3dae13a8a48e
Rautenberg <erautenb@gmail.com>
2024-12-06 14:57:45 -0500

    feat(Day1): Part 1

--- public/src/1.js
+++ public/src/1.js
@@ -3,57 +3,47 @@ parseData(DAY1, (input) => {
   const timeStringDay1 = `Day ${DAY1}, Total Execution Time`;
   console.time(timeStringDay1);
 
+  const formatedInput = sortLists(formatLists(input));
+  console.log(formatedInput)
+
   const timeString1 = `Day ${DAY1}, Part 1 Execution Time`;
   console.time(timeString1);
-  const part1 = getSum(formatLines(input, getNums));
+  const part1 = getDistanceSum(getDistances(formatedInput));
   console.timeEnd(timeString1);
 
   const timeString2 = `Day ${DAY1}, Part 2 Execution Time`;
   console.time(timeString2);
-  const part2 = getSum(formatLines(input, getFormattedNums));
+  const part2 = "";
   console.timeEnd(timeString2);
 
   console.timeEnd(timeStringDay1);
   showAnswers(DAY1, part1, part2);
 });
 
-const formatLines = (lines, callback) => {
-  return lines.map(line => callback(line));
+const formatLists = input => {
+  return input.reduce((acc, curr) => {
+    const [ , a, b] = curr.match(/(\d+).*?(\d+)/);
+    acc[0].push(parseInt(a));
+    acc[1].push(parseInt(b));
+    return acc;
+  }, [ [], [] ]);
 };
 
-const getNums = line => {
-  return line.match(/(\d)/g);
+const sortLists = lists => {
+  return [ lists[0].toSorted(), lists[1].toSorted()];
 };
 
-const getSum = lines => {
-  return lines.reduce((acc, curr) => acc += getLineNum(curr), 0);
+const getDistance = (a, b) => {
+  return Math.abs(a - b);
 }
 
-const getLineNum = line => {
-  return parseInt(`${line[0]}${line[line.length - 1]}`);
-};
-
-const numberWords = {
-  one: 1,
-  two: 2,
-  three: 3,
-  four: 4,
-  five: 5,
-  six: 6,
-  seven: 7,
-  eight: 8,
-  nine: 9
+const getDistances = lists => {
+  return distances = lists[0].reduce((acc, curr, index) => {
+    acc.push(getDistance(curr, lists[1][index]));
+    return acc;
+  }, []);
 }
 
-const getValidNumber = num => {
-  return typeof num === 'number' && !isNaN(num);
+const getDistanceSum = distances => {
+  return distances.reduce((acc, curr) => acc += curr, 0);
 };
-
-const getFormattedNums = line => {
-  const regex = new RegExp(`(?=(\\d|${Object.keys(numberWords).join('|')}))`, 'gi');
-  const splitLine = [...line.matchAll(regex)].map(match => match[1]);
-  return splitLine.map(element => (
-    getValidNumber(parseInt(element)) && element) ||
-    (numberWords[element] && numberWords[element].toString())
-  );
-}
