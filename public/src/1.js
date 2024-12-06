4a41d83ec8f387386945fa98610f8f14b4495577
Rautenberg <erautenb@gmail.com>
2024-12-06 15:18:32 -0500

    feat(Day1): Part 2

--- public/src/1.js
+++ public/src/1.js
@@ -3,8 +3,10 @@ parseData(DAY1, (input) => {
   const timeStringDay1 = `Day ${DAY1}, Total Execution Time`;
   console.time(timeStringDay1);
 
+  const timeStringData1 = `Day ${DAY1}, Data Setup Execution Time`;
+  console.time(timeStringData1);
   const formatedInput = sortLists(formatLists(input));
-  console.log(formatedInput)
+  console.timeEnd(timeStringData1);
 
   const timeString1 = `Day ${DAY1}, Part 1 Execution Time`;
   console.time(timeString1);
@@ -13,7 +15,7 @@ parseData(DAY1, (input) => {
 
   const timeString2 = `Day ${DAY1}, Part 2 Execution Time`;
   console.time(timeString2);
-  const part2 = "";
+  const part2 = getSimilarityScore(formatedInput);
   console.timeEnd(timeString2);
 
   console.timeEnd(timeStringDay1);
@@ -22,7 +24,7 @@ parseData(DAY1, (input) => {
 
 const formatLists = input => {
   return input.reduce((acc, curr) => {
-    const [ , a, b] = curr.match(/(\d+).*?(\d+)/);
+    const [ , a, b] = curr.match(/(\d+)\s*?(\d+)/);
     acc[0].push(parseInt(a));
     acc[1].push(parseInt(b));
     return acc;
@@ -47,3 +49,20 @@ const getDistances = lists => {
 const getDistanceSum = distances => {
   return distances.reduce((acc, curr) => acc += curr, 0);
 };
+
+const countOccurrences = list => {
+  const counts = new Map();
+  for (const num of list) {
+    counts.has(num) ?
+      counts.set(num, counts.get(num) + 1) :
+      counts.set(num, 1);
+  }
+  return counts;
+}
+
+const getSimilarityScore = lists => {
+  const counts = countOccurrences(lists[1]);
+  return lists[0].reduce((acc, curr) =>
+    acc += (curr * counts.get(curr) || 0),
+  0);
+}
