958617b8bf67dbb4c01f8f712b229cd512268d79
Rautenberg <erautenb@gmail.com>
2024-12-07 15:30:09 -0500

    feat(Day4): Part 2

--- public/src/4.js
+++ public/src/4.js
@@ -10,12 +10,12 @@ parseData(DAY4, (input) => {
 
   const timeString1 = `Day ${DAY4}, Part 1 Execution Time`;
   console.time(timeString1);
-  const part1 = getXmasCount(wordSearch);
+  const part1 = getXMASCount(wordSearch);
   console.timeEnd(timeString1);
 
   const timeString2 = `Day ${DAY4}, Part 2 Execution Time`;
   console.time(timeString2);
-  const part2 = '';
+  const part2 = getMASXCount(wordSearch);
   console.timeEnd(timeString2);
 
   console.timeEnd(timeStringDay4);
@@ -29,14 +29,14 @@ const formatWordSearch = input => {
   }, []);
 };
 
-const getXmasCount = wordSearch => {
+const getXMASCount = wordSearch => {
   return wordSearch.reduce((acc, row, r) => {
-    row.forEach((column, c) => acc += checkIfXmas(wordSearch, r, c));
+    row.forEach((column, c) => acc += checkIfXMAS(wordSearch, r, c));
     return acc;
   }, 0);
 };
 
-const OFFSETS = {
+const XMASOFFSETS = {
   forward: [ [0, 1], [0, 2], [0, 3] ],
   backward: [ [0, -1], [0, -2], [0, -3] ],
   down: [ [1, 0], [2, 0], [3, 0] ],
@@ -47,20 +47,54 @@ const OFFSETS = {
   upleft: [ [-1, -1], [-2, -2], [-3, -3] ]
 };
 
-const checkIfXmas = (wordSearch, r, c) => {
+const checkIfXMAS = (wordSearch, r, c) => {
   let count = 0;
   if (wordSearch[r][c] === 'X') {
-    for (const direction in OFFSETS) {
-      if (wordSearch[r + OFFSETS[direction][0][0]] &&
-          wordSearch[r + OFFSETS[direction][1][0]] &&
-          wordSearch[r + OFFSETS[direction][2][0]] &&
-          wordSearch[r + OFFSETS[direction][0][0]][c + OFFSETS[direction][0][1]] === 'M' &&
-          wordSearch[r + OFFSETS[direction][1][0]][c + OFFSETS[direction][1][1]] === 'A' &&
-          wordSearch[r + OFFSETS[direction][2][0]][c + OFFSETS[direction][2][1]] === 'S'
+    for (const direction in XMASOFFSETS) {
+      if (wordSearch[r + XMASOFFSETS[direction][0][0]] &&
+          wordSearch[r + XMASOFFSETS[direction][1][0]] &&
+          wordSearch[r + XMASOFFSETS[direction][2][0]] &&
+          wordSearch[r + XMASOFFSETS[direction][0][0]][c + XMASOFFSETS[direction][0][1]] === 'M' &&
+          wordSearch[r + XMASOFFSETS[direction][1][0]][c + XMASOFFSETS[direction][1][1]] === 'A' &&
+          wordSearch[r + XMASOFFSETS[direction][2][0]][c + XMASOFFSETS[direction][2][1]] === 'S'
           ) {
         count++;
       }
     }
+  }
+  return count;
+};
+
+const getMASXCount = wordSearch => {
+  return wordSearch.reduce((acc, row, r) => {
+    row.forEach((column, c) => acc += checkIfMASX(wordSearch, r, c));
+    return acc;
+  }, 0);
+};
+
+const checkIfMASX = (wordSearch, r, c) => {
+  let count = 0;
+  if (wordSearch[r][c] === 'A') {
+    if (wordSearch[r-1] && wordSearch[r-1][c-1] === 'M' &&
+        wordSearch[r+1] && wordSearch[r+1][c+1] === 'S' &&
+        wordSearch[r-1] && wordSearch[r-1][c+1] === 'M' &&
+        wordSearch[r+1] && wordSearch[r+1][c-1] === 'S'
+      ) count++;
+    if (wordSearch[r-1] && wordSearch[r-1][c-1] === 'S' &&
+        wordSearch[r+1] && wordSearch[r+1][c+1] === 'M' &&
+        wordSearch[r-1] && wordSearch[r-1][c+1] === 'S' &&
+        wordSearch[r+1] && wordSearch[r+1][c-1] === 'M'
+      ) count++;
+    if (wordSearch[r-1] && wordSearch[r-1][c-1] === 'M' &&
+        wordSearch[r+1] && wordSearch[r+1][c+1] === 'S' &&
+        wordSearch[r-1] && wordSearch[r-1][c+1] === 'S' &&
+        wordSearch[r+1] && wordSearch[r+1][c-1] === 'M'
+      ) count++;
+    if (wordSearch[r-1] && wordSearch[r-1][c-1] === 'S' &&
+        wordSearch[r+1] && wordSearch[r+1][c+1] === 'M' &&
+        wordSearch[r-1] && wordSearch[r-1][c+1] === 'M' &&
+        wordSearch[r+1] && wordSearch[r+1][c-1] === 'S'
+      ) count++;
   }
   return count;
 };
