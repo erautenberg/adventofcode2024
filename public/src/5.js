fc63aa5ffef9aa3655b023d6364925a1917742bd
Rautenberg <erautenb@gmail.com>
2024-12-07 22:07:33 -0500

    feat(Day5): Part 2

--- public/src/5.js
+++ public/src/5.js
@@ -5,17 +5,17 @@ parseData(DAY5, (input) => {
 
   const timeStringData1 = `Day ${DAY5}, Data Setup Execution Time`;
   console.time(timeStringData1);
-  const pages = formatPages(input);
+  const pages = separateUpdates(formatPages(input));
   console.timeEnd(timeStringData1);
 
   const timeString1 = `Day ${DAY5}, Part 1 Execution Time`;
   console.time(timeString1);
-  const part1 = getMiddleNumberSum(checkUpdates(pages));
+  const part1 = getMiddleNumberSum(pages.correct);
   console.timeEnd(timeString1);
 
   const timeString2 = `Day ${DAY5}, Part 2 Execution Time`;
   console.time(timeString2);
-  const part2 = '';
+  const part2 = getMiddleNumberSum(fixOrders(pages.pageOrders, pages.incorrect));
   console.timeEnd(timeString2);
 
   console.timeEnd(timeStringDay5);
@@ -39,24 +39,51 @@ const formatPages = input => {
   }, { pageOrders: new Map(), updates: []});
 };
 
-const checkUpdates = ( { pageOrders, updates }) => {
+const separateUpdates = ( { pageOrders, updates }) => {
   return updates.reduce((acc, curr) => {
     let isCorrect = true;
     for (let i=1; i < curr.length; i++) {
       let orders = pageOrders.get(curr[i]);
       if (orders && orders.length) {
-        const containsPrevPage = curr.slice(0, i).filter(page => orders.includes(page));
-        if (containsPrevPage.length) {
+        const prevPages = curr.slice(0, i).filter(page => orders.includes(page));
+        if (prevPages.length) {
           isCorrect = false;
           break;
         }
       }
     }
-    isCorrect && acc.push(curr);
+    isCorrect ? acc.correct.push(curr) : acc.incorrect.push(curr);
     return acc;
-  }, []);
+  }, { pageOrders, correct: [], incorrect: []});
 };
 
 const getMiddleNumberSum = updates => {
   return updates.reduce((acc, curr) => acc += curr[Math.floor(curr.length / 2)], 0);
 };
+
+const fixOrders = (pageOrders, updates) => {
+  return updates.map(pages => fixOrder(pageOrders, pages, 1));
+};
+
+const fixOrder = (pageOrders, pages, i) => {
+  if (i == pages.length) return pages;
+
+  const orders = pageOrders.get(pages[i]);
+  if (orders) {
+    const prevPages = pages.slice(0, i).filter(page => orders.includes(page));
+    if (prevPages.length) {
+      const prevPageIndex = pages.indexOf(prevPages[0]);
+      const newPageOrder = [
+        ...pages.slice(0, prevPageIndex), // beginning to first "mistake"
+        pages[i], // page that needs to move up
+        pages[prevPageIndex], // page that needs to move down ("mistake")
+        ...pages.slice(prevPageIndex+1, i), // pages between "mistake" & page that needs to move up
+        ...pages.slice(i+1) // pages after the page that needs to move up
+      ];
+
+      return fixOrder(pageOrders, newPageOrder, 1);
+    }
+  }
+
+  return fixOrder(pageOrders, pages, ++i);
+}
